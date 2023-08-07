91977ac53ecd559308570906213e29e74c517576
Siddharth <siddu2003oct@gmail.com>
2023-08-07 23:51:54 +0530

    after greninja announcement

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,13 @@
+
 const Projects = () => {
     return(
         <div>
             <h2>This is My Projects Page</h2>
             <p>Hi all... The below are my projects</p>
+
+            <p className="greninja_text">
+                I've decided to put up a greninja's img in all th esites that I make...
+            </p>
         </div>
     )
 };
